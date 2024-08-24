7a8d306640be8dbb87af552385782186df255856
smarmarketing2025 <smartmarketing2025@gmail.com>
2024-08-24 20:47:22 +0500

    init git course name

---
+++ assignment.py
@@ -0,0 +1,64 @@
+# calculate rectangle area
+length = 10
+width = 20
+area = length * width
+print (area)
+
+
+# calculate age
+birthYear = 1988
+currentYear = 2024
+currentAge = currentYear - birthYear
+print (currentAge)
+
+
+# calculate temprature from c to f 
+c = 10
+f = (c * 9/5) + 32
+print (f)
+# calculate temprature from f to c
+f = 45
+c = (f * 5/9) - 32
+print (c)
+
+# calculate area of cube
+lenghtSide = 5
+area = 6 * (lenghtSide) ** 2
+print(area)
+
+
+#calculate the area of circle
+radiusCircle = 2.5 
+pie = 3.1416
+area = pie * (radiusCircle) ** 2
+print(area)
+
+
+#find the percentage of a student
+totalMarks = 570
+obtainedMarks = 517
+percentage = (obtainedMarks / totalMarks) * 100
+print("your percentage is",percentage, "%")
+
+
+#let us have two variables of unknown values
+a = 120 
+inMinutes = a / 60
+print(inMinutes)
+inSeconds = 120 * 60
+print(inSeconds)
+
+#calculates the volume of a cylinder
+heightCylinder = 5.5 
+radiusCylinder = 3.5
+pie = 3.1416
+volume = pie * radiusCylinder**2 * heightCylinder
+print(volume)
+
+#calculate the BMI of a person
+weightPerson = 80 
+heightPerson = 1.5
+secondHeight = heightPerson * heightPerson
+print(secondHeight)
+bmiPerson = weightPerson / secondHeight
+print("Your BMI is",bmiPerson, "%")
